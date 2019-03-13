b993443976d352fd73bd1ba8b7ee93e20869d02d
D3nnycrane <39579951+D3nnycrane@users.noreply.github.com>
2019-03-13 15:44:56 +1300

    Add files via upload
    Edabit Question 1 - Return the sum of 2 numbers.

---
+++ Edabit Answer - Return the Sum of 2 Answers/Edabit Answer - Return the Sum of 2 Answers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Edabit_Answer___Return_the_Sum_of_2_Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Sum(2, 2);
+            Console.WriteLine($"The answer is {result}.");
+            Console.Read();
+
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
