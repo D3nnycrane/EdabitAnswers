87e081e7dc9847f9b9cab4064af62f8064455c67
D3nnycrane <39579951+D3nnycrane@users.noreply.github.com>
2019-03-13 15:53:27 +1300

    Add files via upload

---
+++ Edabit Answer  - Number less than or equal to zero/Edabit Answer  - Number less than or equal to zero/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Edabit_Answer____Number_less_than_or_equal_to_zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The value of 0 is {0}", LessThanOrEqualToZero(0));
+            Console.WriteLine("The value of 5 is {0}", LessThanOrEqualToZero(5));
+            Console.WriteLine("The value of -2 is {0}", LessThanOrEqualToZero(-2));
+
+            Console.ReadLine();
+
+        }
+
+        public static bool LessThanOrEqualToZero(double a)
+        {
+            if (a <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
