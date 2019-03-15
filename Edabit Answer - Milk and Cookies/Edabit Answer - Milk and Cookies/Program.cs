efb6ad4b0ae4ed5d8baef54ad5b1d3e310bec959
D3nnycrane <39579951+D3nnycrane@users.noreply.github.com>
2019-03-15 14:40:41 +1300

    Add files via upload

---
+++ Edabit Answer - Milk and Cookies/Edabit Answer - Milk and Cookies/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Edabit_Answer___Milk_and_Cookies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(TimeForMilkAndCookies(2019, 12, 24));
+            Console.WriteLine(TimeForMilkAndCookies(2019, 08, 22));
+            Console.WriteLine(TimeForMilkAndCookies(1985, 12, 24));
+            Console.Read();
+        }
+
+        public static bool TimeForMilkAndCookies(int year, int month, int day)
+        {
+            if (month == 12 && day == 24)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
