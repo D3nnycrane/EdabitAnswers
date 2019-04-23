e9ee316869f39b0338b2d232566277d8f86c9934
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-04-24 11:49:38 +1200

    Add files via upload

---
+++ Edabit Answer - Return Plus One/Edabit_ReturnPlusOne/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_ReturnPlusOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Addition(3));
+            Console.WriteLine(Addition(12));
+            Console.WriteLine(Addition(1));
+            Console.Read();
+        }
+
+        public static int Addition(int num)
+        {
+            return num + 1;
+        }
+    }
+}
