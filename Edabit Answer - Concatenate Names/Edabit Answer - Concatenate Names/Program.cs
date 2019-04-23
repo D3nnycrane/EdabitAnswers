79a6c55516437555d69ad47e5ccb22e23ae2d0e2
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-04-24 11:59:00 +1200

    Add files via upload

---
+++ Edabit Answer - Concatenate Names/Edabit Answer - Concatenate Names/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Concatenate_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConcatName("Father", "Ted"));
+            Console.WriteLine(ConcatName("Freddie", "Mercury"));
+            Console.WriteLine(ConcatName("Jesus", "Christ"));
+            Console.WriteLine();
+        }
+
+        public static string ConcatName(string firstName, string lastName)
+        {
+            return lastName + ", " + firstName;
+        }
+    }
+}
