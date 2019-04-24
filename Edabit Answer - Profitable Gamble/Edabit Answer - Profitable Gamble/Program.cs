81d8822c92e47b358c666a0485f04aab2a2d07e5
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-04-24 12:36:19 +1200

    Add files via upload

---
+++ Edabit Answer - Profitable Gamble/Edabit Answer - Profitable Gamble/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Profitable_Gamble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ProfitableGamble(12, 1, 11));
+            Console.WriteLine(ProfitableGamble(13, 2, 29));
+            Console.Read();
+        }
+
+        public static bool ProfitableGamble(double prob, int prize, double pay)
+        {
+            if ((prob * prize) > pay)
+                return true;
+            else
+                return false;
+        }
+    }
+}
