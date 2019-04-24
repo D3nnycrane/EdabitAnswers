b172e2fefa2cc2c5121f955883562fefb3775f89
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-04-24 12:41:51 +1200

    Add files via upload

---
+++ Edabit Answer - Flip the Boolean/Edabit Answer - Flip the Boolean/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Flip_the_Boolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(true));
+            Console.WriteLine(Reverse(true));
+            Console.WriteLine(Reverse(false));
+            Console.WriteLine(Reverse(false));
+            Console.WriteLine(Reverse(true));
+            Console.Read();
+        }
+
+        public static bool Reverse(bool boolean)
+        {
+            if (boolean == true)
+                return false;
+            else
+                return true;
+        }
+    }
+}
