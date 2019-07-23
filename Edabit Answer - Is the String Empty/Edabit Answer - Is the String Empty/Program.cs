bb918fce14e6e7e7d2cbd2e6c9ac65f048a08a53
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-24 09:08:37 +1200

    Add files via upload

---
+++ Edabit Answer - Is the String Empty/Edabit Answer - Is the String Empty/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Is_the_String_Empty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isEmpty(""));
+            Console.WriteLine(isEmpty("Not Empty"));
+            Console.WriteLine(isEmpty(" "));
+            Console.WriteLine(isEmpty(""));
+            Console.ReadLine();
+        }
+
+        public static bool isEmpty(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+                return true;
+            else
+                return false;
+        }
+    }
+}
