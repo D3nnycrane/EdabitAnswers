885a8012e29c9c4748d1079a3bfb1d1efdd178e6
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-24 08:54:21 +1200

    Add files via upload

---
+++ Edabit Answer - Return Remainder/Edabit Answer - Return Remainder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Return_Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Remainder(4, 9));
+            Console.WriteLine(Remainder(2, 11));
+            Console.WriteLine(Remainder(3, 19));
+            Console.ReadLine();
+        }
+
+        public static int Remainder(int x, int y)
+        {
+            int z = x % y;
+            return z;
+        }
+    }
+}
