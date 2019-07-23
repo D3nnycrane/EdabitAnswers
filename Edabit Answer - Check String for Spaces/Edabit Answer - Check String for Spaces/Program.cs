38c6fc015e71608ab3cc7bea35b951da9911f030
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-24 09:58:50 +1200

    Add files via upload

---
+++ Edabit Answer - Check String for Spaces/Edabit Answer - Check String for Spaces/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Check_String_for_Spaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HasSpaces("nospaceshere"));
+            Console.WriteLine(HasSpaces("Spaces right here!"));
+            Console.WriteLine(HasSpaces("NOSPACESHERE"));
+            Console.WriteLine(HasSpaces(" "));
+            Console.ReadLine();
+        }
+
+        public static bool HasSpaces(string str)
+        {
+            if (str.Contains(" "))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
