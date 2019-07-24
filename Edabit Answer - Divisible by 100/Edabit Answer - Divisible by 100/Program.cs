a17a285f3f2128682c9506f06af749d9d0d41d38
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-25 09:33:42 +1200

    Add files via upload

---
+++ Edabit Answer - Divisible by 100/Edabit Answer - Divisible by 100/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Divisible_by_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divisible(100));
+            Console.WriteLine(Divisible(101));
+            Console.WriteLine(Divisible(1000));
+            Console.WriteLine(Divisible(7));
+            Console.WriteLine(Divisible(10000));
+            Console.Read();
+        }
+
+        public static bool Divisible(int number)
+        {
+            
+            if (number % 100 == 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
