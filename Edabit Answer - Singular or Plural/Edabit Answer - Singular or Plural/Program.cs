6835ab732431783760648080acc4af84743f3049
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-25 09:51:25 +1200

    Add files via upload

---
+++ Edabit Answer - Singular or Plural/Edabit Answer - Singular or Plural/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Singular_or_Plural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPlural("Egg"));
+            Console.WriteLine(IsPlural("Eggs"));
+            Console.WriteLine(IsPlural("Chicken"));
+            Console.WriteLine(IsPlural("Chickens"));
+            Console.Read();
+        }
+
+        public static bool IsPlural(string word)
+        {
+            if (word.EndsWith("s"))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
