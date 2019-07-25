27566bf52ab833986e749bc5c49537d2be51f79f
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-26 11:14:19 +1200

    Add files via upload

---
+++ Edabit Answer - To The Power Of/Edabit Answer - To The Power Of/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___To_The_Power_Of
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateExponent(5, 5));
+            Console.WriteLine(CalculateExponent(10, 10));
+            Console.WriteLine(CalculateExponent(3, 3));
+            Console.Read();
+        }
+
+        public static long CalculateExponent(long number, long exponent)
+        {
+            return (long) (Math.Pow(number, exponent));
+        }
+    }
+}
---
+++ Edabit Answer - To The Power Of/Edabit Answer - To The Power Of/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Edabit Answer - To The Power Of")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Edabit Answer - To The Power Of")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2aa39db7-c03e-4f41-ad95-3d6486a3bc26")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
