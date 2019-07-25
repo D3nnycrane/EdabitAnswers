3162e0c864732c16ce49ee232c5dd6787b0f8417
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-25 12:59:57 +1200

    Add files via upload

---
+++ Edabit Answer - Convert Number to Dashes/Edabit - Convert Number to Dashes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit___Convert_Number_to_Dashes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Go(5));
+            Console.WriteLine(Go(3));
+            Console.WriteLine(Go(15));
+            Console.WriteLine(Go(9));
+            Console.WriteLine(Go(11));
+            Console.Read();
+        }
+
+        public static string Go(int num)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('-', num);
+            return sb.ToString();
+        }
+    }
+}
