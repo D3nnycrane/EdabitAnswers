41284aafb299b29c5707ac56f5a7477ebe45115d
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-25 14:14:41 +1200

    Add files via upload

---
+++ Edabit Answer - Multiply By Length/Edabit Answer - Multiply By Length/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Multiply_By_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int[] MultiplyByLength(int[] arr)
+        {
+            var multiplier = arr.Length;
+            return arr.Select(x => x * multiplier).ToArray();
+        }
+    }
+}
