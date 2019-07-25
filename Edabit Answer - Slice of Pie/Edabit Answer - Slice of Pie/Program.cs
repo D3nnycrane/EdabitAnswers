de5c1cb0439b589b164f3a9e4ef894d3943bcac2
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-25 15:43:03 +1200

    Add files via upload

---
+++ Edabit Answer - Slice of Pie/Edabit Answer - Slice of Pie/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Slice_of_Pie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EqualSlices(11, 5, 2));
+            Console.WriteLine(EqualSlices(11, 5, 3));
+            Console.WriteLine(EqualSlices(8, 3, 2));
+            Console.WriteLine(EqualSlices(8, 3, 3));
+            Console.WriteLine(EqualSlices(24, 12, 2));
+            Console.Read();
+        }
+
+        public static bool EqualSlices(int total, int people, int each)
+        {
+            var result = people * each;
+            if (result <= total)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
