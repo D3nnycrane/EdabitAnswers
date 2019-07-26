f2c2797bf49d75f95b11b7cdf48fc2ee37086043
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-26 13:26:01 +1200

    Add files via upload

---
+++ Edabit Answer - Remove First Last Char/Edabit Answer - Remove First Last Char/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Remove_First_Last_Char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveFirstLast("hello"));
+            Console.WriteLine(RemoveFirstLast("maybe"));
+            Console.WriteLine(RemoveFirstLast("benefit"));
+            Console.WriteLine(RemoveFirstLast("a"));
+            Console.Read();
+
+        }
+
+        public static string RemoveFirstLast(string str)
+        {
+            if (str.Length <= 2)
+                return str;
+
+            string myString = "";
+            myString = str.Substring(1, str.Length - 2);
+            return myString;
+        }
+    }
+}
