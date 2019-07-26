bd5f5951e2e1882da94e28c7c25f2c116014781e
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-26 13:31:07 +1200

    Add files via upload

---
+++ Edabit Answers - Hacker Speak/Edabit Answers - Hacker Speak/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answers___Hacker_Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HackerSpeak("javascript is cool"));
+            Console.WriteLine(HackerSpeak("programming is fun"));
+            Console.WriteLine(HackerSpeak("become a coder"));
+            Console.Read();
+        }
+
+        public static string HackerSpeak(string str)
+        {
+            return str.Replace("a", "4").Replace("e", "3").Replace("i", "1").Replace("o", "0").Replace("s", "5");
+        }
+    }
+}
