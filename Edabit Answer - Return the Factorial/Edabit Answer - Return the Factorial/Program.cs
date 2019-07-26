d1b168e55bd460e5da7ef25f1cf76f3cac0b47a3
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-26 12:09:33 +1200

    Add files via upload

---
+++ Edabit Answer - Return the Factorial/Edabit Answer - Return the Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Return_the_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(3));
+            Console.WriteLine(Factorial(5));
+            Console.WriteLine(Factorial(13));
+            Console.Read();
+        }
+
+        public static int Factorial(int num)
+        {
+            int fact = num;
+            for (int i = num - 1; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+}
