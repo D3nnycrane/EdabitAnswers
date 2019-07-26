93c0407a5a4d11c525430a1d75c050faf576c37f
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-07-26 13:14:20 +1200

    Add files via upload

---
+++ Edabit Answer - Count Syllables/Edabit Answer - Count Syllables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Count_Syllables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberSyllables("buf-fet"));
+            Console.WriteLine(NumberSyllables("beau-ti-ful"));
+            Console.WriteLine(NumberSyllables("mon-u-men-tal"));
+            Console.WriteLine(NumberSyllables("on-o-mat-o-poe-ia"));
+            Console.Read();
+        }
+
+        public static int NumberSyllables(string word)
+        {
+            int count = 0;
+            string[] split = word.Split('-');
+            foreach (string s in split)
+            {
+                if (s.Trim() != "")
+                    count++;
+            }
+            return count;
+        }
+    }
+}
