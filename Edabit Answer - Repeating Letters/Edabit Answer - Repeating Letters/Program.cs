06cfc662218b7ea62d5847b51cbf0d7125183003
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-08-06 08:53:41 +1200

    Add files via upload

---
+++ Edabit Answer - Repeating Letters/Edabit Answer - Repeating Letters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Repeating_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DoubleChar("Tester"));
+            Console.WriteLine(DoubleChar("Cowabunga"));
+            Console.WriteLine(DoubleChar("Adinosinetriphosphate"));
+            Console.WriteLine(DoubleChar("Dweeb"));
+            Console.Read();
+        }
+
+        public static string DoubleChar(string str)
+        {
+            return String.Concat(str.Select(c => c.ToString() + c.ToString()));
+        }
+    }
+}
