2e6931fcb91534281b1b6ad80b9ab1305e53cbc9
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-08-06 09:39:06 +1200

    Add files via upload

---
+++ Edabit Answer - Area of a Triangle/Edabit Answer - Area of a Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Area_of_a_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(triArea(3, 2));
+            Console.WriteLine(triArea(7, 4));
+            Console.WriteLine(triArea(10, 10));
+            Console.Read();
+        }
+
+        public static int triArea(int b, int h)
+        {
+            return (b * h) / 2;
+        }
+    }
+}
