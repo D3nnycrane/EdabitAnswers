304d8301ff3743e2260a6aa924cf31cdf1645607
Mark <39579951+D3nnycrane@users.noreply.github.com>
2019-08-06 08:56:44 +1200

    Add files via upload

---
+++ Edabit Answer - Integer Divisible by 5/Edabit Answer - Integer Divisible by 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_Answer___Integer_Divisible_by_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(divisibleByFive(5));
+            Console.WriteLine(divisibleByFive(-55));
+            Console.WriteLine(divisibleByFive(37));
+            Console.Read();
+        }
+
+        public static bool divisibleByFive(int n)
+        {
+            bool result = (n % 5 == 0) ? true : false;
+            return result;
+        }
+    }
+}
